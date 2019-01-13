f84e5896e16a56c85ff57f3815f82d8bc28195ff
Adalberto Claudio Quiros <aclaudioquiros@gmail.com>
2019-01-13 12:43:40 +0000

    Fix on data to use the right database name and marker

--- preparation/data.py
+++ preparation/data.py
@@ -3,15 +3,19 @@ from preparation.dataset import Dataset
 
 
 class Data:
-    def __init__(self, patch_h, patch_w, n_channels, batch_size, project_path=os.getcwd(), thresholds=()):
+    def __init__(self, dataset, marker, patch_h, patch_w, n_channels, batch_size, project_path=os.getcwd(), thresholds=()):
 
         # Directories and file name handling.
-        relative_dataset_path = os.path.join('dataset', 'nki_he')
-        dataset_path = os.path.join(project_path, relative_dataset_path)
+        self.dataset = dataset
+        self.marker = marker
+        self.dataset_name = '%s_%s' % (self.dataset, self.marker)
+        relative_dataset_path = os.path.join(self.dataset, self.marker)
+        relative_dataset_path = os.path.join('dataset', relative_dataset_path)
+        relative_dataset_path = os.path.join(project_path, relative_dataset_path)
         self.pathes_path = os.path.join(relative_dataset_path, 'patches_h%s_w%s' % (patch_h, patch_w))
 
-        self.hdf5_train = os.path.join(self.pathes_path, 'hdf5_nki_he_train.h5')
-        self.hdf5_test = os.path.join(self.pathes_path, 'hdf5_nki_he_test.h5')
+        self.hdf5_train = os.path.join(self.pathes_path, 'hdf5_%s_train.h5' % self.dataset_name)
+        self.hdf5_test = os.path.join(self.pathes_path, 'hdf5_%s_test.h5' % self.dataset_name)
 
         # Train dataset
         self.training = Dataset(self.hdf5_train, patch_h, patch_w, n_channels, batch_size=batch_size, data_type='train', thresholds=thresholds)
