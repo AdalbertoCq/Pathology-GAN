454a13fef4590c820301a5108bab86b99b94b48c
Adalberto Claudio Quiros <aclaudioquiros@gmail.com>
2019-01-28 17:56:48 +0000

    Refactoring code to split loss, optimizer from model definition.

---
+++ models/generative/loss.py
@@ -0,0 +1,120 @@
+import tensorflow as tf
+
+def losses(loss_type, output_fake, output_real, logits_fake, logits_real, real_images=None, fake_images=None, discriminator=None, batch_size=None, gp_coeff=None, display=True):
+
+    # Variable to track which loss function is actually used.
+    loss_print = ''
+    if 'relativistic' in loss_type:
+        logits_diff_real_fake = logits_real - tf.reduce_mean(logits_fake, axis=0, keepdims=True)
+        logits_diff_fake_real = logits_fake - tf.reduce_mean(logits_real, axis=0, keepdims=True)
+        loss_print += 'relativistic '
+
+        if 'standard' in loss_type:
+            # Discriminator loss.
+            loss_dis_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_diff_real_fake, labels=tf.ones_like(logits_fake)))
+            loss_dis_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_diff_fake_real, labels=tf.zeros_like(logits_fake)))
+            loss_dis = loss_dis_real + loss_dis_fake
+
+            # Generator loss.
+            loss_gen_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_diff_fake_real, labels=tf.ones_like(logits_fake)))
+            loss_gen_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_diff_real_fake, labels=tf.zeros_like(logits_fake)))
+            loss_gen = loss_gen_real + loss_gen_fake
+            loss_print += 'standard '
+
+        elif 'least square' in loss_type:
+            # Discriminator loss.
+            loss_dis_real = tf.reduce_mean(tf.square(logits_diff_real_fake-1.0))
+            loss_dis_fake = tf.reduce_mean(tf.square(logits_diff_fake_real+1.0))
+            loss_dis = loss_dis_real + loss_dis_fake
+
+            # Generator loss.
+            loss_gen_real = tf.reduce_mean(tf.square(logits_diff_fake_real-1.0))
+            loss_gen_fake = tf.reduce_mean(tf.square(logits_diff_real_fake+1.0))
+            loss_gen = loss_gen_real + loss_gen_fake
+            loss_print += 'least square '
+
+        elif 'wasserstein distance' in loss_type:
+            # Calculating X hat.
+            epsilon = tf.random.uniform(shape=(batch_size, 1, 1, 1), minval=0.0, maxval=1.0, dtype=tf.float32, name='epsilon')
+            x_gp = real_images*(1-epsilon) + fake_images*epsilon
+            output_gp, logits_gp = discriminator(x_gp, True)
+
+            # Calculating Gradient Penalty.
+            grad_gp = tf.gradients(logits_gp, x_gp)
+            l2_grad_gp = tf.sqrt(tf.reduce_sum(tf.square(grad_gp), axis=[1, 2, 3]))
+            grad_penalty= tf.reduce_sum(tf.square(l2_grad_gp-1.0))
+
+            # Discriminator loss.
+            loss_dis_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_diff_real_fake, labels=tf.ones_like(logits_fake)))
+            loss_dis_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_diff_fake_real, labels=tf.zeros_like(logits_fake)))
+            loss_dis = loss_dis_real + loss_dis_fake + (gp_coeff*grad_penalty)
+
+            # Generator loss.
+            loss_gen_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_diff_fake_real, labels=tf.ones_like(logits_fake)))
+            loss_gen_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_diff_real_fake, labels=tf.zeros_like(logits_fake)))
+            loss_gen = loss_gen_real + loss_gen_fake
+            loss_print += 'wasserstein distance '
+
+    elif loss_type == 'standard':
+        # Discriminator loss. Uses hinge loss on discriminator.
+        loss_dis_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_fake, labels=tf.zeros_like(output_fake)))
+        loss_dis_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_real, labels=tf.ones_like(output_fake)*0.9))
+        loss_dis = loss_dis_fake + loss_dis_real
+
+        # Generator loss.
+        # This is where we implement -log[D(G(z))] instead log[1-D(G(z))].
+        # Recall the implementation of cross-entropy, sign already in. 
+        loss_gen = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_fake, labels=tf.ones_like(output_fake)))
+        loss_print += 'standard '
+
+    elif loss_type == 'least square':       
+        # Discriminator loss.
+        loss_dis_fake = tf.reduce_mean(tf.square(output_fake))
+        loss_dis_real = tf.reduce_mean(tf.square(output_real-1.0))
+        loss_dis = 0.5*(loss_dis_fake + loss_dis_real)
+
+        # Generator loss.
+        loss_gen = 0.5*tf.reduce_mean(tf.square(output_fake-1.0))
+        loss_print += 'least square '
+
+    elif 'wasserstein distance' in loss_type:
+        # Discriminator loss.
+        loss_dis_real = tf.reduce_mean(logits_real)
+        loss_dis_fake = tf.reduce_mean(logits_fake)
+        loss_dis = -loss_dis_real + loss_dis_fake
+        loss_print += 'wasserstein distance '
+
+        # Generator loss.
+        loss_gen = -loss_dis_fake
+        if 'gradient penalty' in loss_type:
+            # Calculating X hat.
+            epsilon = tf.random.uniform(shape=(batch_size, 1, 1, 1), minval=0.0, maxval=1.0, dtype=tf.float32, name='epsilon')
+            x_gp = real_images*(1-epsilon) + fake_images*epsilon
+            output_gp, logits_gp = discriminator(x_gp, True)
+
+            # Calculating Gradient Penalty.
+            grad_gp = tf.gradients(logits_gp, x_gp)
+            l2_grad_gp = tf.sqrt(tf.reduce_sum(tf.square(grad_gp), axis=[1, 2, 3]))
+            grad_penalty= tf.reduce_sum(tf.square(l2_grad_gp-1.0))
+            loss_dis += (gp_coeff*grad_penalty)
+            loss_print += 'gradient penalty '
+        
+    elif loss_type == 'hinge':
+
+        loss_dis_real = tf.reduce_mean(min(0, -1 + logits_real))
+        loss_dis_fake = tf.reduce_mean(min(0, -1 - logits_fake))
+        loss_dis = - (loss_dis_fake + loss_dis_real)
+
+        loss_gen = - loss_dis_fake
+        loss_print += 'hinge '
+
+    else:
+        print('Loss %s not defined' % loss_type)
+        exit(1)
+
+    if display:
+        print('Loss: %s' % loss_print)
+
+    return loss_dis, loss_gen
+
+
---
+++ models/generative/optimizer.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+
+def optimizer(beta_1, loss_gen, loss_dis, loss_type, learning_rate_input, beta_2=None, clipping=None, display=True):
+    trainable_variables = tf.trainable_variables()
+    generator_variables = [variable for variable in trainable_variables if variable.name.startswith('generator')]
+    discriminator_variables = [variable for variable in trainable_variables if variable.name.startswith('discriminator')]
+
+    # Optimizer variable to track with optimizer is actually used.
+    optimizer_print = ''
+
+    # Handling Batch Normalization.
+    with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+        if 'wasserstein distance' in loss_type and 'gradient penalty' in loss_type:
+            train_discriminator = tf.train.AdamOptimizer(learning_rate_input, beta_1, beta_2).minimize(loss_dis, var_list=discriminator_variables)
+            train_generator = tf.train.AdamOptimizer(learning_rate_input, beta_1, beta_2).minimize(loss_gen, var_list=generator_variables)
+            optimizer_print += 'Wasserstein Distance Gradient penalty - AdamOptimizer'
+        #TODO Fix this for RMSProp.
+        elif 'wasserstein distance' in loss_type and 'gradient penalty' not in loss_type:
+            # Weight Clipping on Discriminator, this is done to ensure the Lipschitz constrain.
+            train_discriminator = tf.train.AdamOptimizer(learning_rate_input, beta_1, beta_2).minimize(loss_dis, var_list=discriminator_variables)
+            dis_weight_clipping = [value.assign(tf.clip_by_value(value, -clipping, clipping)) for value in discriminator_variables]
+            train_discriminator = tf.group(*[train_discriminator, dis_weight_clipping])
+
+            train_generator = tf.train.AdamOptimizer(learning_rate_input, beta_1, beta_2).minimize(loss_gen, var_list=generator_variables)
+            optimizer_print += 'Wasserstein Distance - AdamOptimizer'
+
+            '''
+            RMS_optimizer_dis = tf.train.RMSPropOptimizer(learning_rate=learning_rate)
+            train_discriminator = RMS_optimizer_dis.minimize(loss_dis , var_list=discriminator_variables)
+            
+            # Weight Clipping on Discriminator, this is done to ensure the Lipschitz constrain.
+            dis_weight_clipping = [value.assign(tf.clip_by_value(value, -c, c)) for value in discriminator_variables]
+            
+            train_discriminator = tf.group(*[train_discriminator, dis_weight_clipping])
+            
+            # Generator.
+            RMS_optimizer_gen = tf.train.RMSPropOptimizer(learning_rate=learning_rate)
+            train_generator = RMS_optimizer_gen.minimize(loss_gen, var_list=generator_variables)
+            optimizer_print += 'Wassertein Distance - RMSPropOptimizer'
+            '''
+
+        elif loss_type == 'standard' or loss_type == 'least square' or 'relativistic' in loss_type:
+            train_generator = tf.train.AdamOptimizer(learning_rate=learning_rate_input, beta1=beta_1).minimize(loss_gen, var_list=generator_variables)
+            train_discriminator = tf.train.AdamOptimizer(learning_rate=learning_rate_input, beta1=beta_1).minimize(loss_dis, var_list=discriminator_variables) 
+            optimizer_print += '%s - AdamOptimizer' % loss_type
+        else:
+            print('Loss %s not defined' % loss_type)
+            exit(1)
+
+        if display:
+            print('Optimizer: %s' % optimizer_print)
+            
+    return train_discriminator, train_generator
