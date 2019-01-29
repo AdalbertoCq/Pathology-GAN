Fix spectral normalization

models/generative/ops.py
@@ -36,29 +36,34 @@ def attention_block(x, i):
         o = tf.reshape(o, shape=x.get_shape())
         y = gamma*o + x
 
-        return y
+    return y
 
-def spectral_normalization(weight_matrix, power_iterations):
-    shape = weight_matrix.get_shape()
-    print(shape)
+def spectral_normalization(filter, power_iterations):
     # Vector is preserved after each SGD iteration, good performance with power_iter=1 and presenving. 
     # Need to make v trainable, and stop gradient descent to going through this path/variables.
     # Isotropic gaussian. 
-    u_shape = (shape[-1], 1)
-    u = tf.get_variable('u', shape=u_shape, dtype=tf.float32, initializer=tf.truncated_normal_initializer(), trainable=True)
+
+    filter_shape = filter.get_shape()
+    filter_reshape = tf.reshape(filter, [-1, filter_shape[-1]])
+    
+    u_shape = (1, filter_shape[-1])
+    # If I put trainable = False, I don't need to use tf.stop_gradient()
+    u = tf.get_variable('u', shape=u_shape, dtype=tf.float32, initializer=tf.truncated_normal_initializer(), trainable=False)
 
     u_norm = u
     v_norm = None
+    
     for i in range(power_iterations):
-        v = tf.matmul(tf.transpose(weight_matrix), u_norm)
-        v_norm = tf.math.l2_normalize(x=v, epsilon=1e-12)
-        u = tf.matmul(weight_matrix, v_nom)
-        u_norm = tf.math.l2_normalize(x=u, epsilon=1e-12)
+        v_iter = tf.matmul(u_norm, tf.transpose(filter_reshape))
+        v_norm = tf.math.l2_normalize(x=v_iter, epsilon=1e-12)
+        u_iter = tf.matmul(v_norm, filter_reshape)
+        u_norm = tf.math.l2_normalize(x=u_iter, epsilon=1e-12)
 
-    u_norm = tf.stop_gradient(u_norm)
-    v_norm = tf.stop_gradient(v_norm)
+    # How do I verify this?
+    # u_norm = tf.stop_gradient(u_norm)
+    # v_norm = tf.stop_gradient(v_norm)
 
-    singular_w = tf.matmul(tf.transpose(u_norm), tf.matmul(weight_matrix, v_norm))
+    singular_w = tf.matmul(tf.matmul(v_norm, filter_reshape), tf.transpose(u_norm))
     '''
     tf.assign(ref,  value):
         This operation outputs a Tensor that holds the new value of 'ref' after the value has been assigned. 
@@ -73,52 +78,64 @@ def spectral_normalization(weight_matrix, power_iterations):
     The kernel of the conv it's a variable it self, with its dependencies.
     '''
     with tf.control_dependencies([u.assign(u_norm)]):
-        weight_normalized = weight_matrix / singular_w
-        weight_normalized = tf.reshape(weight_normalized, weight_normalized)
-    return weight_normalized
+        filter_normalized = filter / singular_w
+        filter_normalized = tf.reshape(filter_normalized, filter.get_shape())
+    return filter_normalized
 
 
-def convolutional(inputs, output_channels, filter_size, stride, padding, conv_type, data_format='NHWC', output_shape=None, spectral=False, power_iterations=None):
-    
-    current_shape = inputs.get_shape()
-    input_channels = current_shape[3]
-    
-    # Kernel and bias initilization.
-    weight_init = tf.contrib.layers.xavier_initializer()
-    filter_shape = (filter_size, filter_size, input_channels, output_channels)    
-    if 'transpose'in conv_type or 'upscale' in conv_type:
-        filter_shape = (filter_size, filter_size, output_channels, input_channels)    
-
-    filter = tf.get_variable('filter', filter_shape, initializer=weight_init)    
-    if spectral:
-    	filter = spectral_normalization(filter, power_iterations)
-    b = tf.get_variable('bias', [1, 1, 1, output_channels], initializer=tf.constant_initializer(0))
-    
-    # Type of convolutional operation.
-    if conv_type == 'upscale':
-        output_shape = [tf.shape(inputs)[0], current_shape[1]*2, current_shape[2]*2, output_channels]
-        # Weight filter initializer.
-        filter = tf.pad(filter, ([1,1], [1,1], [0,0], [0,0]), mode='CONSTANT')
-        filter = tf.add_n([filter[1:,1:], filter[:-1,1:], filter[1:,:-1], filter[:-1,:-1]])
-        strides = [1, 2, 2, 1]
-        output = tf.nn.conv2d_transpose(input=inputs, filter=filter, output_shape=tf.stack(output_shape), strides=strides, padding=padding, data_format=data_format)
+def convolutional(inputs, output_channels, filter_size, stride, padding, conv_type, scope, data_format='NHWC', output_shape=None, spectral=False, power_iterations=None):
+    with tf.variable_scope('conv_layer_%s' % scope):
+        current_shape = inputs.get_shape()
+        input_channels = current_shape[3]
         
-    elif conv_type == 'downscale':
-        # Weight filter initializer.
-        filter = tf.pad(filter, ([1,1], [1,1], [0,0], [0,0]), mode='CONSTANT')
-        filter = tf.add_n([filter[1:,1:], filter[:-1,1:], filter[1:,:-1], filter[:-1,:-1]])
-        strides = [1, 2, 2, 1]
-        output = tf.nn.conv2d(input=inputs, filter=filter, strides=strides, padding=padding, data_format=data_format)
+        # Kernel and bias initilization.
+        weight_init = tf.contrib.layers.xavier_initializer()
+        filter_shape = (filter_size, filter_size, input_channels, output_channels)    
+        if 'transpose'in conv_type or 'upscale' in conv_type:
+            filter_shape = (filter_size, filter_size, output_channels, input_channels)    
+
+        filter = tf.get_variable('filter', filter_shape, initializer=weight_init)    
+        if spectral:
+        	filter = spectral_normalization(filter, power_iterations)
+        b = tf.get_variable('bias', [1, 1, 1, output_channels], initializer=tf.constant_initializer(0))
         
-    elif conv_type == 'transpose':
-        output_shape = [tf.shape(inputs)[0], current_shape[1]*stride, current_shape[2]*stride, output_channels]
-        strides = [1, stride, stride, 1]
-        output = tf.nn.conv2d_transpose(value=inputs, filter=filter, output_shape=tf.stack(output_shape), strides=strides, padding=padding, data_format=data_format)
-    
-    elif conv_type == 'convolutional':
-        strides = [1, stride, stride, 1]
-        output = tf.nn.conv2d(input=inputs, filter=filter, strides=strides, padding=padding, data_format=data_format)
-    
-    output += b
-    
+        # Type of convolutional operation.
+        if conv_type == 'upscale':
+            output_shape = [tf.shape(inputs)[0], current_shape[1]*2, current_shape[2]*2, output_channels]
+            # Weight filter initializer.
+            filter = tf.pad(filter, ([1,1], [1,1], [0,0], [0,0]), mode='CONSTANT')
+            filter = tf.add_n([filter[1:,1:], filter[:-1,1:], filter[1:,:-1], filter[:-1,:-1]])
+            strides = [1, 2, 2, 1]
+            output = tf.nn.conv2d_transpose(value=inputs, filter=filter, output_shape=tf.stack(output_shape), strides=strides, padding=padding, data_format=data_format)
+            
+        elif conv_type == 'downscale':
+            # Weight filter initializer.
+            filter = tf.pad(filter, ([1,1], [1,1], [0,0], [0,0]), mode='CONSTANT')
+            filter = tf.add_n([filter[1:,1:], filter[:-1,1:], filter[1:,:-1], filter[:-1,:-1]])
+            strides = [1, 2, 2, 1]
+            output = tf.nn.conv2d(input=inputs, filter=filter, strides=strides, padding=padding, data_format=data_format)
+            
+        elif conv_type == 'transpose':
+            output_shape = [tf.shape(inputs)[0], current_shape[1]*stride, current_shape[2]*stride, output_channels]
+            strides = [1, stride, stride, 1]
+            output = tf.nn.conv2d_transpose(value=inputs, filter=filter, output_shape=tf.stack(output_shape), strides=strides, padding=padding, data_format=data_format)
+        
+        elif conv_type == 'convolutional':
+            strides = [1, stride, stride, 1]
+            output = tf.nn.conv2d(input=inputs, filter=filter, strides=strides, padding=padding, data_format=data_format)
+        
+        output += b
     return output
+
+
+def dense(inputs, out_dim, scope, use_bias=True, spectral=False, power_iterations=None):
+    with tf.variable_scope('dense_layer_%s' % scope):
+        in_dim = inputs.get_shape()[-1]
+        weights = tf.get_variable('kernel', shape=[in_dim, out_dim], dtype=tf.float32)
+        output = tf.matmul(inputs, spectral_normalization(weights, power_iterations))
+        if use_bias : 
+            bias = tf.get_variable("bias", [out_dim], initializer=tf.constant_initializer(0.0))
+        output += bias
+    return output
+
+