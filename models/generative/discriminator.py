b190139ee8df727cefe5f91aff6a74c69e6eb302
Adalberto Claudio Quiros <aclaudioquiros@gmail.com>
2019-02-26 18:05:18 +0000

    ResNet GAN, discriminator, and generator.

--- models/generative/discriminator.py
+++ models/generative/discriminator.py
@@ -1,71 +1,86 @@
 import tensorflow as tf
 from models.generative.ops import *
 from models.generative.activations import *
+from models.generative.normalization import *
 
+display = True
 
-def discriminator_resnet(images, layers, use_bn, spectral, activation, reuse):
+def discriminator_resnet(images, layers, spectral, activation, reuse, normalization=None):
 	net = images
 	channels = [32, 64, 128, 256, 512, 1024]
 
+	if display:
+		print('Discriminator Information.')
+		print('Channels: ', channels[:layers])
+		print('Normalization: ', normalization)
+		print('Activation: ', activation)
 	with tf.variable_scope('discriminator', reuse=reuse):
 		for layer in range(layers):
-			net = residual_block(inputs=net, filter_size=3, stride=1, padding='SAME', channels=channels[layer], scope=layer, is_training=True, use_bn=use_bn, use_bias=True, 
+			# ResBlock.
+			net = residual_block(inputs=net, filter_size=3, stride=1, padding='SAME', scope=layer, is_training=True, normalization=normalization, use_bias=True, 
 								 spectral=spectral, activation=activation)
-			net = convolutional(inputs=net, output_channels=channels[layer], filter_size=5, stride=2, padding='SAME', conv_type='convolutional', spectral=spectral, scope=layer)
-			if use_bn: net = tf.layers.batch_normalization(inputs=net, training=True)
+			if display: 
+				print('ResBlock Layer: channels %s filter_size=3, stride=1, padding=SAME, conv_type=transpose scope=%s Output Shape: %s' % (channels[layer], layer, net.shape))
+
+			# Down.
+			net = convolutional(inputs=net, output_channels=channels[layer], filter_size=4, stride=2, padding='SAME', conv_type='convolutional', spectral=spectral, scope=layer)
+			if normalization is not None: net = normalization(inputs=net, training=True)
 			net = activation(net)
+			if display: 
+				print('Conv Layer: channels %s filter_size=4, stride=2, padding=SAME, conv_type=transpose scope=%s Output Shape: %s' % (channels[layer], layer, net.shape))
 
 		# Flatten.
 		net = tf.layers.flatten(inputs=net)
 
 		# Dense.
 		net = dense(inputs=net, out_dim=channels[-1], spectral=spectral, scope=1)				
-		if use_bn: net = tf.layers.batch_normalization(inputs=net, training=True)
+		if normalization is not None: net = normalization(inputs=net, training=True)
 		net = activation(net)
-		# Shape = (None, 1024)
+		if display: 
+			print('Dense Layer: Dim=%s Output Shape: %s' % (channels[-1], net.shape))
 
 		# Dense
 		logits = dense(inputs=net, out_dim=1, spectral=spectral, scope=2)				
-		# Shape = (None, 1)
-		output = signmoid(logits)
+		output = sigmoid(logits)
+		if display: 
+			print('Dense Layer: Dim=1 Output Shape: %s' % net.shape)
 
+	print()
 	return output, logits
 
 
-def discriminator_cnn(images, layers, use_bn, spectral, activation, reuse):
+def discriminator(images, layers, spectral, activation, reuse, normalization=None):
 	net = images
 	channels = [32, 64, 128, 256, 512, 1024]
 	
-	print('Discriminator.')
-	print('Channels: ', channels[:layers])
+	if display:
+		print('Discriminator Information.')
+		print('Channels: ', channels[:layers])
+		print('Normalization: ', normalization)
+		print('Activation: ', activation)
 	with tf.variable_scope('discriminator', reuse=reuse):
 		# Padding = 'Same' -> H_new = H_old // Stride
 
-		# Input Shape = (None, 224, 224, 3)
 		for layer in range(layers):
-			# Conv.
-			print('Conv Layer: channels %s ilter_size=5, stride=2, padding=SAME, conv_type=transpose scope=%s' % (channels[layer], layer))
-			net = convolutional(inputs=net, output_channels=channels[layer], filter_size=5, stride=2, padding='SAME', conv_type='convolutional', spectral=spectral, scope=layer)
-			# net = attention_block(x=net, i=1)
-			if use_bn: net = tf.layers.batch_normalization(inputs=net, training=True)
+			# Down.
+			if display: print('Conv Layer: channels %s filter_size=5, stride=2, padding=SAME, conv_type=transpose scope=%s' % (channels[layer], layer))
+			net = convolutional(inputs=net, output_channels=channels[layer], filter_size=5, stride=2, padding='SAME', conv_type='convolutional', spectral=spectral, scope=layer+1)
+			if normalization is not None: net = normalization(inputs=net, training=True)
 			net = activation(net)
-			# Shape = (None, 112, 112, 32)
 
 		# Flatten.
 		net = tf.layers.flatten(inputs=net)
-		# Shape = (None, 7*7*512)
-
+		
 		# Dense.
+		if display: print('Dense Layer: Dim=%s' % channels[-1])
 		net = dense(inputs=net, out_dim=channels[-1], spectral=spectral, scope=1)				
-		if use_bn: net = tf.layers.batch_normalization(inputs=net, training=True)
+		if normalization is not None: net = normalization(inputs=net, training=True)
 		net = activation(net)
-		# Shape = (None, 1024)
-
+		
 		# Dense
+		if display: print('Dense Layer: Dim=1')
 		logits = dense(inputs=net, out_dim=1, spectral=spectral, scope=2)				
-		# Shape = (None, 1)
-		output = tf.nn.sigmoid(x=logits)
+		output = sigmoid(logits)
 
-		# Padding = 'Same' -> H_new = H_old // Stride
 	print()
 	return output, logits
--- models/generative/generator.py
+++ models/generative/generator.py
@@ -1,92 +1,108 @@
 import tensorflow as tf
 from models.generative.ops import *
 from models.generative.activations import *
+from models.generative.normalization import *
 
-def generator_resnet(z_input, image_channels, layers, spectral, activation, reuse, is_train):
-	channels = [32, 64, 128, 256, 512, 1024]
-	reversed_channel = reversed(channels[:layers])
+display = True
 
-	with tf.variable_scope('generator', reuse=reuse):
-		# Doesn't work ReLU, tried.
-		# Input Shape = (None, 100)
-		# Dense.			
-		net = dense(inputs=z_input, out_dim=1024, spectral=spectral, scope=1)				
-		net = tf.layers.batch_normalization(inputs=net, training=is_train)
-		net = activation(net)
-		# Shape = (None, 1024)
-
-		# Dense.
-		net = dense(inputs=net, out_dim=256*7*7, spectral=spectral, scope=2)				
-		net = tf.layers.batch_normalization(inputs=net, training=is_train)
-		net = activation(net)
-		# Shape = (None, 256*7*7)
-
-		# Reshape
-		net = tf.reshape(tensor=net, shape=(-1, 7, 7, 256), name='reshape')
-		# Shape = (None, 7, 7, 256)
-
-		for layer in range(layers):
-			net = residual_block(inputs=net, filter_size=3, stride=1, padding='SAME', channels=reversed_channel[layer], scope=layer, is_training=is_train, use_bn=True,
-								 spectral=spectral, activation=activation)
-
-			# Conv.
-			net = convolutional(inputs=net, output_channels=reversed_channel[layer], filter_size=5, stride=1, padding='SAME', conv_type='convolutional', spectral=spectral, scope=2*(layers+1))
-			net = tf.layers.batch_normalization(inputs=net, training=is_train)
-			net = activation(net)
-
-		net = convolutional(inputs=net, output_channels=image_channels, filter_size=3, stride=1, padding='SAME', conv_type='convolutional', spectral=spectral, scope=2*(layers+1))
-		net = sigmoid(net)
-
-
-def generator(z_input, image_channels, layers, spectral, activation, reuse, is_train):
+def generator_resnet(z_input, image_channels, layers, spectral, activation, reuse, is_train, normalization):
 	channels = [32, 64, 128, 256, 512, 1024]
 	reversed_channel = list(reversed(channels[:layers]))
 
-	print('Generator.')
-	print('Channels: ', channels[:layers])
-	
+	if display:
+		print('Generator Information.')
+		print('Channels: ', channels[:layers])
+		print('Normalization: ', normalization)
+		print('Activation: ', activation)
+
 	with tf.variable_scope('generator', reuse=reuse):
 		# Doesn't work ReLU, tried.
-		# Input Shape = (None, 100)
+
 		# Dense.			
 		net = dense(inputs=z_input, out_dim=1024, spectral=spectral, scope=1)				
-		net = tf.layers.batch_normalization(inputs=net, training=is_train)
+		net = normalization(inputs=net, training=is_train)
 		net = activation(net)
-		# Shape = (None, 1024)
+		if display: 
+			print('Dense Layer: Dim=1024 Output Shape: %s' % net.shape)
 
 		# Dense.
 		net = dense(inputs=net, out_dim=256*7*7, spectral=spectral, scope=2)				
-		net = tf.layers.batch_normalization(inputs=net, training=is_train)
+		net = normalization(inputs=net, training=is_train)
 		net = activation(net)
-		# Shape = (None, 256*7*7)
+		if display: 
+			print('Dense Layer: Dim=256*7*7 Output Shape: %s' % net.shape)
 
 		# Reshape
 		net = tf.reshape(tensor=net, shape=(-1, 7, 7, 256), name='reshape')
-		# Shape = (None, 7, 7, 256)
 
 		for layer in range(layers):
-			# Conv.
-			print('Conv Layer: channels %s ilter_size=2, stride=2, padding=SAME, conv_type=transpose scope=%s' % (reversed_channel[layer], 2*(layer+1)-1))
-			net = convolutional(inputs=net, output_channels=reversed_channel[layer], filter_size=2, stride=2, padding='SAME', conv_type='transpose', spectral=spectral, scope=2*(layer+1)-1)
-			net = tf.layers.batch_normalization(inputs=net, training=is_train)
+			# ResBlock.
+			net = residual_block(inputs=net, filter_size=3, stride=1, padding='SAME', scope=layer, is_training=is_train, spectral=spectral,
+								 activation=activation, normalization=normalization)
+			if display:
+				print('ResBlock Layer: channels %s filter_size=3, stride=1, padding=SAME, conv_type=transpose scope=%s Output Shape: %s' % (reversed_channel[layer], layer, net.shape))
+
+			# Up.
+			net = convolutional(inputs=net, output_channels=reversed_channel[layer], filter_size=2, stride=2, padding='SAME', conv_type='transpose', spectral=spectral, scope=layer)
+			net = normalization(inputs=net, training=is_train)
 			net = activation(net)
-			# Shape = (None, 14, 14, 256)
+			if display:
+				print('Conv Layer: channels %s ilter_size=2, stride=2, padding=SAME, conv_type=transpose scope=%s Output Shape: %s' % (reversed_channel[layer], layer, net.shape))
 
-			if layer != len(range(layers))-1:
-				# Conv.
-				print('Conv Layer: channels %s ilter_size=5, stride=1, padding=SAME, conv_type=convolutional scope=%s' % (reversed_channel[layer+1], 2*(layer+1)))
-				net = convolutional(inputs=net, output_channels=reversed_channel[layer+1], filter_size=5, stride=1, padding='SAME', conv_type='convolutional', spectral=spectral, scope=2*(layer+1))
-				net = tf.layers.batch_normalization(inputs=net, training=is_train)
-				net = activation(net)
+		logits = convolutional(inputs=net, output_channels=image_channels, filter_size=3, stride=1, padding='SAME', conv_type='convolutional', spectral=spectral, scope=layer+1)
+		output = sigmoid(logits)
+		if display: 
+			print('Logits Layer: channels %s ilter_size=3, stride=1, padding=SAME, conv_type=convolutional scope=%s Output Shape: %s' % (image_channels, layer+1, net.shape))
 
-		# Conv.
-		print('Logits Layer: channels %s ilter_size=5, stride=1, padding=SAME, conv_type=convolutional scope=%s' % (image_channels, 2*(layer+1)))
-		logits = convolutional(inputs=net, output_channels=image_channels, filter_size=2, stride=2, padding='SAME', conv_type='transpose', spectral=spectral, scope=2*(layer+1))
-		# Shape = (None, 448, 448, 3)
-		output = tf.nn.sigmoid(x=logits, name='output')
-	
 	print()
 	return output
 
+def generator(z_input, image_channels, layers, spectral, activation, reuse, is_train, normalization):
+	channels = [32, 64, 128, 256, 512, 1024]
+	reversed_channel = list(reversed(channels[:layers]))
 
+	if display:
+		print('Generator Information.')
+		print('Channels: ', channels[:layers])
+		print('Normalization: ', normalization)
+		print('Activation: ', activation)
 	
+	with tf.variable_scope('generator', reuse=reuse):
+		# Doesn't work ReLU, tried.
+		
+		# Dense.
+		if display: print('Dense Layer: Dim=1024')
+		net = dense(inputs=z_input, out_dim=1024, spectral=spectral, scope=1)				
+		net = normalization(inputs=net, training=is_train)
+		net = activation(net)
+
+		# Dense.
+		if display: print('Dense Layer: Dim=256*7*7')
+		net = dense(inputs=net, out_dim=256*7*7, spectral=spectral, scope=2)				
+		net = normalization(inputs=net, training=is_train)
+		net = activation(net)
+
+		# Reshape
+		net = tf.reshape(tensor=net, shape=(-1, 7, 7, 256), name='reshape')
+
+		for layer in range(layers):
+			# Conv.
+			if display: print('Conv Layer: channels %s filter_size=2, stride=2, padding=SAME, conv_type=transpose scope=%s' % (reversed_channel[layer], 2*(layer+1)-1))
+			net = convolutional(inputs=net, output_channels=reversed_channel[layer], filter_size=2, stride=2, padding='SAME', conv_type='transpose', spectral=spectral, scope=2*(layer+1)-1)
+			net = normalization(inputs=net, training=is_train)
+			net = activation(net)
+
+			if layer != len(range(layers))-1:
+				# Conv.
+				if display: print('Conv Layer: channels %s filter_size=5, stride=1, padding=SAME, conv_type=convolutional scope=%s' % (reversed_channel[layer+1], 2*(layer+1)))
+				net = convolutional(inputs=net, output_channels=reversed_channel[layer+1], filter_size=5, stride=1, padding='SAME', conv_type='convolutional', spectral=spectral, scope=2*(layer+1))
+				net = normalization(inputs=net, training=is_train)
+				net = activation(net)
+
+		# Conv.
+		if display: print('Logits Layer: channels %s filter_size=2, stride=2, padding=SAME, conv_type=convolutional scope=%s' % (image_channels, 2*(layer+1)))
+		logits = convolutional(inputs=net, output_channels=image_channels, filter_size=2, stride=2, padding='SAME', conv_type='transpose', spectral=spectral, scope=2*(layer+1))
+		output = sigmoid(logits)
+	
+	print()
+	return output
