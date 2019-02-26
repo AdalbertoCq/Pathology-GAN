3645922fb5d533f38cc53af841ea3760e6536cac
Adalberto Claudio Quiros <aclaudioquiros@gmail.com>
2019-02-26 18:06:03 +0000

    option to use different normalization types.

--- models/generative/ops.py
+++ models/generative/ops.py
@@ -263,13 +263,14 @@ def dense(inputs, out_dim, scope, use_bias=True, spectral=False, power_iteration
     return output
 
 
-def residual_block(inputs, filter_size, stride, padding, channels, scope, is_training=True, use_bn=False, use_bias=True, spectral=False, activation=None, power_iterations=1):
+def residual_block(inputs, filter_size, stride, padding, scope, is_training=True, normalization=None, use_bias=True, spectral=False, activation=None, power_iterations=1):
+    channels = inputs.shape.as_list()[-1]
     with tf.variable_scope('resblock_%s' % scope):
         with tf.variable_scope('part_1'):
             # Convolutional
             net = convolutional(inputs, channels, filter_size, stride, padding, 'convolutional', scope=1, spectral=spectral, power_iterations=power_iterations)
             # Batch Normalization
-            if use_bn: net = tf.layers.batch_normalization(inputs=net, training=is_training)
+            if normalization is not None: net = normalization(inputs=net, training=is_training)
             # Activation
             if activation is not None: net = activation(net)
             
@@ -277,7 +278,7 @@ def residual_block(inputs, filter_size, stride, padding, channels, scope, is_tra
             # Convolutional
             net = convolutional(net, channels, filter_size, stride, padding, 'convolutional', scope=1, spectral=spectral, power_iterations=power_iterations)
             # Batch Normalization
-            if use_bn: net = tf.layers.batch_normalization(inputs=net, training=is_training)
+            if normalization is not None: net = normalization(inputs=net, training=is_training)
             # Activation
             if activation is not None: net = activation(net)
             
