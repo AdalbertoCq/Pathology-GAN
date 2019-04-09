b2221d7ff3bf9bd46614bdc4d7470c4b79674c99
Adalberto Claudio Quiros <aclaudioquiros@gmail.com>
2019-04-09 15:47:08 +0100

    Update run_gans.py

--- run_gans.py
+++ run_gans.py
@@ -8,16 +8,20 @@ from models.generative.tools import *
 from data_manipulation.utils import write_sprite_image
 from tensorflow.contrib.tensorboard.plugins import projector
 from models.generative.utils import *
-from models.generative.gans.InfoSNGAN import InfoSNGAN
 import platform
 
 
-model = 'infoSNGAN'
+from models.generative.gans.InfoSAGAN import InfoSAGAN
+model = 'InfoSAGAN'
+
+from models.generative.gans.SAGAN import SAGAN
+model = 'SAGAN'
+
 
 if platform.system() == 'Linux':
-    main_path = '/home/adalberto/Documents/Cancer_TMA_Generative/data model output/' + model
+    main_path = '/home/adalberto/Documents/Cancer_TMA_Generative/data_model_output/' + model
 elif platform.system() == 'Darwin':
-    main_path = '/Users/adalbertoclaudioquiros/Documents/Code/UofG/PhD/Cancer_TMA_Generative/data model output/' + model
+    main_path = '/Users/adalbertoclaudioquiros/Documents/Code/UofG/PhD/Cancer_TMA_Generative/data_model_output/' + model
     
 # Dataset information.
 image_width = 224
@@ -25,23 +29,28 @@ image_height = 224
 image_channels = 3
 dataset='nki'
 marker='he'
-name_run = 'h%s_w%s_n%s_cdim_30' % (image_height, image_width, image_channels)
+name_run = 'h%s_w%s_n%s' % (image_height, image_width, image_channels)
 data_out_path = '%s/%s' % (main_path, name_run)
 
 # Hyperparameters.
 epochs = 60
 batch_size = 64
 z_dim = 100
-c_dim = 30
-learning_rate_g = 1e-4
+c_dim = 20
+conditional = True
+label_dim = 5
+learning_rate_g = 5e-5
 learning_rate_d = 1e-4
 alpha = 0.2
 beta_1 = 0.5
 beta_2 = 0.9
 n_critic = 5
 gp_coeff = .5
-delta = 1.
+delta = 2.
 use_bn = False
+# loss_type='relativistic standard'
+loss_type = 'hinge'
+
 
 restore = False
 
@@ -49,6 +58,11 @@ data = Data(dataset=dataset, marker=marker, patch_h=image_height, patch_w=image_
 
 # with tf.device("/gpu:0"):
 with tf.Graph().as_default():
-    infosngan = InfoSNGAN(data=data, z_dim=z_dim, c_dim=c_dim, use_bn=use_bn, alpha=alpha, beta_1=beta_1, learning_rate_g=learning_rate_g, learning_rate_d=learning_rate_d, delta=delta,
-                         power_iterations=1, beta_2=beta_2, n_critic=n_critic, gp_coeff=gp_coeff, loss_type='relativistic gradient penalty infogan')
-    losses = infosngan.train(epochs, data_out_path, data, restore, n_images=10, show_epochs=None)
+    # infosagan = InfoSAGAN(data=data, z_dim=z_dim, use_bn=use_bn, alpha=alpha, beta_1=beta_1, learning_rate_g=learning_rate_g, learning_rate_d=learning_rate_d, power_iterations=1, 
+    #               		  c_dim=c_dim, beta_2=beta_2, n_critic=n_critic, gp_coeff=gp_coeff, delta=delta, loss_type='relativistic gradient penalty infogan')
+    # losses = infosagan.train(epochs, data_out_path, data, restore, n_images=10, show_epochs=None)
+
+    sagan = SAGAN(data=data, z_dim=z_dim, use_bn=use_bn, alpha=alpha, beta_1=beta_1, learning_rate_g=learning_rate_g, learning_rate_d=learning_rate_d, beta_2=beta_2,
+                  n_critic=n_critic, gp_coeff=gp_coeff, conditional=conditional, label_dim=label_dim, loss_type=loss_type)
+
+    losses = sagan.train(epochs, data_out_path, data, restore, n_images=10, show_epochs=None)
