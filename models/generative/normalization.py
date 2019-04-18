6dba904dfc0b7f3baeadc25ac20491dd39689169
Adalberto Claudio Quiros <aclaudioquiros@gmail.com>
2019-04-18 19:06:48 +0100

    Inclusion of CBN

--- models/generative/normalization.py
+++ models/generative/normalization.py
@@ -23,10 +23,15 @@ def group_norm(inputs, training, c=None, spectral=False, scope=False):
 	return output
 
 def conditional_instance_norm(inputs, training, c, scope, spectral=False):
-
-	batch, height, width, channels = inputs.shape.as_list()
+	input_dims = inputs.shape.as_list()
+	if len(input_dims) == 4:
+		batch, height, width, channels = input_dims
+	else:
+		batch, channels = input_dims
 
 	with tf.variable_scope('conditional_instance_norm_%s' % scope):
+		decay = 0.9
+		epsilon = 1e-5
 
 		# MLP for gamma, and beta.
 		inter_dim = int((channels+c.shape.as_list()[-1])/2)
@@ -35,11 +40,56 @@ def conditional_instance_norm(inputs, training, c, scope, spectral=False):
 		gamma = dense(inputs=net, out_dim=channels, scope='gamma', spectral=spectral, display=False)
 		gamma = ReLU(gamma)
 		beta = dense(inputs=net, out_dim=channels, scope='beta', spectral=spectral, display=False)
-
-		gamma = tf.expand_dims(tf.expand_dims(gamma, 1), 1)
-		beta = tf.expand_dims(tf.expand_dims(beta, 1), 1)
+		if  len(input_dims) == 4:
+			gamma = tf.expand_dims(tf.expand_dims(gamma, 1), 1)
+			beta = tf.expand_dims(tf.expand_dims(beta, 1), 1)
 		
-		mean, variance = tf.nn.moments(inputs, axes=[1,2], keep_dims=True)
-		batch_norm_output = tf.nn.batch_normalization(inputs, mean, variance, beta, gamma, 1e-10)
+		if  len(input_dims) == 4:
+			batch_mean, batch_variance = tf.nn.moments(inputs, axes=[1,2], keep_dims=True)
+		else:
+			batch_mean, batch_variance = tf.nn.moments(inputs, axes=[1], keep_dims=True)
+
+		batch_norm_output = tf.nn.batch_normalization(inputs, batch_mean, batch_variance, beta, gamma, epsilon)
 
 	return batch_norm_output
+
+def conditional_batch_norm(inputs, training, c, scope, spectral=False):
+	input_dims = inputs.shape.as_list()
+	if len(input_dims) == 4:
+		batch, height, width, channels = input_dims
+	else:
+		batch, channels = input_dims
+
+	with tf.variable_scope('conditional_batch_norm_%s' % scope) :
+		decay = 0.9
+		epsilon = 1e-5
+
+		test_mean = tf.get_variable("pop_mean", shape=[channels], dtype=tf.float32, initializer=tf.constant_initializer(0.0), trainable=False)
+		test_variance = tf.get_variable("pop_var", shape=[channels], dtype=tf.float32, initializer=tf.constant_initializer(1.0), trainable=False)
+
+		# MLP for gamma, and beta.
+		inter_dim = int((channels+c.shape.as_list()[-1])/2)
+		net = dense(inputs=c, out_dim=inter_dim, scope=1, spectral=spectral, display=False)
+		net = ReLU(net)
+		gamma = dense(inputs=net, out_dim=channels, scope='gamma', spectral=spectral, display=False)
+		gamma = ReLU(gamma)
+		beta = dense(inputs=net, out_dim=channels, scope='beta', spectral=spectral, display=False)
+		if  len(input_dims) == 4:
+			gamma = tf.expand_dims(tf.expand_dims(gamma, 1), 1)
+			beta = tf.expand_dims(tf.expand_dims(beta, 1), 1)
+
+		if training:
+			if  len(input_dims) == 4:
+				batch_mean, batch_variance = tf.nn.moments(inputs, axes=[0, 1, 2])
+				# batch_mean, batch_variance = tf.nn.moments(inputs, axes=[0, 1, 2], keep_dims=True)
+			else:
+				batch_mean, batch_variance = tf.nn.moments(inputs, axes=[0, 1])
+				# batch_mean, batch_variance = tf.nn.moments(inputs, axes=[0, 1], keep_dims=True)
+			ema_mean = tf.assign(test_mean, test_mean * decay + batch_mean * (1 - decay))
+			ema_variance = tf.assign(test_variance, test_variance * decay + batch_variance * (1 - decay))
+			with tf.control_dependencies([ema_mean, ema_variance]):
+				batch_norm_output = tf.nn.batch_normalization(inputs, batch_mean, batch_variance, beta, gamma, epsilon)
+		else:
+			batch_norm_output = tf.nn.batch_normalization(inputs, test_mean, test_variance, beta, gamma, epsilon)
+	return batch_norm_output
+
