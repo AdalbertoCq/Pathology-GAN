a412a120fd0593ee819fdf86af0c5aff8d80e56b
Adalberto Claudio Quiros <aclaudioquiros@gmail.com>
2019-04-18 19:07:38 +0100

    Create combine_data.py

---
+++ combine_data.py
@@ -0,0 +1,14 @@
+from data_manipulation.combine_databases import Combine_databases
+import platform
+
+if platform.system() == 'Linux':
+    main_path = '/home/adalberto/Documents/Cancer_TMA_Generative'
+elif platform.system() == 'Darwin':
+    main_path = '/Users/adalbertoclaudioquiros/Documents/Code/UofG/PhD/Cancer_TMA_Generative'
+
+marker = 'he'
+
+
+for value in [224]:
+    with Combine_databases(patch_h=value, patch_w=value, n_channels=3, datasets=['vgh', 'nki'], marker=marker, save_img=True, project_path=main_path) as preprocessor:
+    	preprocessor.run()
