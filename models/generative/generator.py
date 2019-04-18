78fabe9b3ba93a0edfa66ca4d88d231de98c5728
Adalberto Claudio Quiros <aclaudioquiros@gmail.com>
2019-04-18 19:06:22 +0100

    CBN changes

--- models/generative/generator.py
+++ models/generative/generator.py
@@ -1,13 +1,28 @@
 import tensorflow as tf
+import math
 from models.generative.ops import *
 from models.generative.activations import *
 from models.generative.normalization import *
 
+
 display = True
 
-def generator_resnet(z_input, image_channels, layers, spectral, activation, reuse, is_train, normalization, init='xavier', regularizer=None, cond_label=None, attention=None, up='upscale'):
+def generator_resnet(z_input, image_channels, layers, spectral, activation, reuse, is_train, normalization, init='xavier', regularizer=None, cond_label=None, attention=None, up='upscale', bigGAN=False):
 	channels = [32, 64, 128, 256, 512, 1024]
 	reversed_channel = list(reversed(channels[:layers]))
+
+	# Question here: combine z dims for upscale and the conv after, or make them independent.
+	if bigGAN:
+		z_dim = z_input.shape.as_list()[-1]
+		blocks = 2 + layers
+		block_dims = math.floor(z_dim/blocks)
+		remainder = z_dim - block_dims*blocks
+		if remainder == 0:
+			z_sets = [block_dims]*(blocks + 1)
+		else:
+			z_sets = [block_dims]*blocks + [remainder]
+		z_splits = tf.split(z_input, num_or_size_splits=z_sets, axis=-1)
+
 
 	if display:
 		print('GENERATOR INFORMATION:')
@@ -18,25 +33,44 @@ def generator_resnet(z_input, image_channels, layers, spectral, activation, reus
 		print()
 
 	with tf.variable_scope('generator', reuse=reuse):
-		# Doesn't work ReLU, tried.
+		if bigGAN: 
+			z_input_block = z_splits[0]
+			label = z_splits[1]
+		else:
+			z_input_block = z_input
+			label = z_input
+		if cond_label is not None: label = tf.concat([cond_label, label], axis=-1)
 
 		# Dense.			
-		net = dense(inputs=z_input, out_dim=1024, spectral=spectral, init=init, regularizer=regularizer, scope=1)			
-		net = batch_norm(inputs=net, training=is_train)
+		net = dense(inputs=z_input_block, out_dim=1024, spectral=spectral, init=init, regularizer=regularizer, scope=1)			
+		# net = batch_norm(inputs=net, training=is_train)
+		# Introducing instance norm into dense layers.
+		net = normalization(inputs=net, training=is_train, c=label, spectral=spectral, scope='dense_1')
 		net = activation(net)
-		
+
+		if bigGAN: label = z_splits[2]
+		else: label = z_input
+		if cond_label is not None: label = tf.concat([cond_label, label], axis=-1)
+
 		# Dense.
 		net = dense(inputs=net, out_dim=256*7*7, spectral=spectral, init=init, regularizer=regularizer, scope=2)				
-		net = batch_norm(inputs=net, training=is_train)
+		# net = batch_norm(inputs=net, training=is_train)
+		# Introducing instance norm into dense layers.
+		net = normalization(inputs=net, training=is_train, c=label, spectral=spectral, scope='dense_2')
 		net = activation(net)
 		
 		# Reshape
 		net = tf.reshape(tensor=net, shape=(-1, 7, 7, 256), name='reshape')
 
 		for layer in range(layers):
+
+			if bigGAN: label = z_splits[3+layer] 
+			else: label = z_input
+			if cond_label is not None: label = tf.concat([cond_label, label], axis=-1)
+
 			# ResBlock.
 			net = residual_block(inputs=net, filter_size=3, stride=1, padding='SAME', scope=layer, is_training=is_train, spectral=spectral, init=init, regularizer=regularizer, 
-								 activation=activation, normalization=normalization, cond_label=cond_label)
+								 activation=activation, normalization=normalization, cond_label=label)
 			
 			# Attention layer. 
 			if attention is not None and net.shape.as_list()[1]==attention:
@@ -44,7 +78,7 @@ def generator_resnet(z_input, image_channels, layers, spectral, activation, reus
 			
 			# Up.
 			net = convolutional(inputs=net, output_channels=reversed_channel[layer], filter_size=2, stride=2, padding='SAME', conv_type=up, spectral=spectral, init=init, regularizer=regularizer, scope=layer)
-			net = normalization(inputs=net, training=is_train, c=cond_label, spectral=spectral, scope=layer)
+			net = normalization(inputs=net, training=is_train, c=label, spectral=spectral, scope=layer)
 			net = activation(net)
 			
 		logits = convolutional(inputs=net, output_channels=image_channels, filter_size=3, stride=1, padding='SAME', conv_type='convolutional', spectral=spectral, init=init, regularizer=regularizer, scope='logits')
