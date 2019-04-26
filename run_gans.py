0ab88d05c12e25ae2edaed634174a6182492f5ed
Adalberto Claudio Quiros <aclaudioquiros@gmail.com>
2019-04-26 18:06:20 +0100

    Update run_gans.py

--- run_gans.py
+++ run_gans.py
@@ -31,22 +31,23 @@ name_run = 'h%s_w%s_n%s' % (image_height, image_width, image_channels)
 data_out_path = '%s/%s' % (main_path, name_run)
 
 # Hyperparameters.
-epochs = 60
+epochs = 15
 batch_size = 64
 learning_rate_g = 1e-4
 learning_rate_d = 1e-4
 beta_1 = 0.5
 beta_2 = 0.9
-restore = False
+restore = True
 
 # Model
+layers = 5
 z_dim = 128
 alpha = 0.2
 n_critic = 5
 gp_coeff = .5
 use_bn = False
 loss_type = 'relativistic gradient penalty'
-# loss_type = 'relativistic'
+# loss_type = 'relativistic standard'
 # loss_type = 'hinge'
 
 # Conditional
@@ -65,7 +66,7 @@ with tf.Graph().as_default():
     # biggan = InfoBigGAN(data=data, z_dim=z_dim, use_bn=use_bn, alpha=alpha, beta_1=beta_1, learning_rate_g=learning_rate_g, learning_rate_d=learning_rate_d, beta_2=beta_2,
     #               		c_dim=c_dim, delta=delta, n_critic=n_critic, gp_coeff=gp_coeff, conditional=conditional, label_dim=label_dim, loss_type=loss_type)
 
-    biggan = BigGAN(data=data, z_dim=z_dim, use_bn=use_bn, alpha=alpha, beta_1=beta_1, learning_rate_g=learning_rate_g, learning_rate_d=learning_rate_d, beta_2=beta_2,
+    biggan = BigGAN(data=data, z_dim=z_dim, layers=layers, use_bn=use_bn, alpha=alpha, beta_1=beta_1, learning_rate_g=learning_rate_g, learning_rate_d=learning_rate_d, beta_2=beta_2,
                   	n_critic=n_critic, gp_coeff=gp_coeff, conditional=conditional, label_dim=label_dim, label_t=label_t, loss_type=loss_type)
 
     losses = biggan.train(epochs, data_out_path, data, restore, n_images=10, show_epochs=None)
