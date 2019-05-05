872cd051f177809ff5627cb64ca30a898bbd2a6a
Adalberto Claudio Quiros <aclaudioquiros@gmail.com>
2019-05-05 12:07:40 +0100

    Update run_gans.py

--- run_gans.py
+++ run_gans.py
@@ -9,11 +9,8 @@ from data_manipulation.utils import write_sprite_image
 from tensorflow.contrib.tensorboard.plugins import projector
 from models.generative.utils import *
 import platform
-from models.generative.gans.InfoBigGAN import InfoBigGAN
 from models.generative.gans.BigGAN import BigGAN
-from models.generative.gans.SAGAN import SAGAN
 
-model = 'SAGAN'
 model = 'BigGAN'
 
 if platform.system() == 'Linux':
@@ -25,19 +22,19 @@ elif platform.system() == 'Darwin':
 image_width = 224
 image_height = 224
 image_channels = 3
-dataset='vgh_nki'
+dataset='nki_vgh'
 marker='he'
 name_run = 'h%s_w%s_n%s' % (image_height, image_width, image_channels)
 data_out_path = '%s/%s' % (main_path, name_run)
 
 # Hyperparameters.
-epochs = 15
-batch_size = 64
+epochs = 30
+batch_size = 70
 learning_rate_g = 1e-4
 learning_rate_d = 1e-4
 beta_1 = 0.5
 beta_2 = 0.9
-restore = True
+restore = False
 
 # Model
 layers = 5
@@ -47,26 +44,16 @@ n_critic = 5
 gp_coeff = .5
 use_bn = False
 loss_type = 'relativistic gradient penalty'
-# loss_type = 'relativistic standard'
-# loss_type = 'hinge'
 
 # Conditional
-conditional = True
-label_dim = 1
+conditional = False
+label_dim = 0
 label_t = 'cat'
 
-# InfoGAN
-delta = 1.0
-c_dim = 10
-
-
-data = Data(dataset=dataset, marker=marker, patch_h=image_height, patch_w=image_width, n_channels=image_channels, batch_size=batch_size)
+data = Data(dataset=dataset, marker=marker, patch_h=image_height, patch_w=image_width, n_channels=image_channels, batch_size=batch_size, labels=False)
 
 with tf.Graph().as_default():
-    # biggan = InfoBigGAN(data=data, z_dim=z_dim, use_bn=use_bn, alpha=alpha, beta_1=beta_1, learning_rate_g=learning_rate_g, learning_rate_d=learning_rate_d, beta_2=beta_2,
-    #               		c_dim=c_dim, delta=delta, n_critic=n_critic, gp_coeff=gp_coeff, conditional=conditional, label_dim=label_dim, loss_type=loss_type)
-
     biggan = BigGAN(data=data, z_dim=z_dim, layers=layers, use_bn=use_bn, alpha=alpha, beta_1=beta_1, learning_rate_g=learning_rate_g, learning_rate_d=learning_rate_d, beta_2=beta_2,
                   	n_critic=n_critic, gp_coeff=gp_coeff, conditional=conditional, label_dim=label_dim, label_t=label_t, loss_type=loss_type)
 
-    losses = biggan.train(epochs, data_out_path, data, restore, n_images=10, show_epochs=None)
+    losses = biggan.train(epochs, data_out_path, data, restore, n_images=10, show_epochs=None, evaluation=30)
