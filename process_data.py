ee3fcb3eb4fc9be160717a731e74b4f5efacd770
Adalberto Claudio Quiros <aclaudioquiros@gmail.com>
2019-07-03 19:16:30 +0100

    Update process_data.py

--- process_data.py
+++ process_data.py
@@ -1,20 +1,15 @@
 from data_manipulation.preprocessor import Preprocessor
-import platform
 
-if platform.system() == 'Linux':
-    main_path = '/home/adalberto/Documents/Cancer_TMA_Generative'
-elif platform.system() == 'Darwin':
-    main_path = '/Users/adalbertoclaudioquiros/Documents/Code/UofG/PhD/Cancer_TMA_Generative'
 
-main_path = '/media/adalberto/Disk2/Cancer_TMA_Generative'
 
 # dataset = 'nki'
 # marker = 'he'
 # labels = 'nki_survival.csv'
 
-marker = 'cathepsin_l'
-for dataset in ['stanford']:
-	labels = None
-	for value in [224]:
-	    with Preprocessor(patch_h=value, patch_w=value, n_channels=3, dataset=dataset, marker=marker,  labels=labels, overlap=True, save_img=True, threshold=240, project_path=main_path) as preprocessor:
+main_path = '/home/adalberto/Documents/Cancer_TMA_Generative'
+marker = 'he'
+for dataset in ['nki']:
+	labels = 'nki_survival.csv'
+	for value in [448]:
+	    with Preprocessor(patch_h=value, patch_w=value, n_channels=3, dataset=dataset, marker=marker,  labels=labels, overlap=True, save_img=True, threshold=215, project_path=main_path) as preprocessor:
 	        preprocessor.run()
