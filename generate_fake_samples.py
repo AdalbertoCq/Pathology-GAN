075792968c755ac371753e889db9a29efe045e00
Adalberto Claudio Quiros <aclaudioquiros@gmail.com>
2020-02-03 19:00:42 +0000

    MIDL 2020 improvements

--- generate_fake_samples.py
+++ generate_fake_samples.py
@@ -6,22 +6,23 @@ from models.generative.gans.PathologyGAN import PathologyGAN
 from models.evaluation.features import *
 
 
-parser = argparse.ArgumentParser(description='StylePathologyGAN fake image generator and feature extraction.')
-parser.add_argument('--checkpoint', dest='checkpoint', required= True, help='Path to pre-trained weights (.ckt) of PathologyGAN: unconditional, er, or survival.')
-parser.add_argument('--num_samples', dest='num_samples', required= True, type=int, help='Number of images to generate.')
-parser.add_argument('--batch_size', dest='batch_size', required= True, type=int, help='Batch size.')
+parser = argparse.ArgumentParser(description='PathologyGAN fake image generator.')
+parser.add_argument('--checkpoint', dest='checkpoint', required= True, help='Path to pre-trained weights (.ckt) of PathologyGAN.')
+parser.add_argument('--num_samples', dest='num_samples', required= True, type=int, default=5000, help='Number of images to generate.')
+parser.add_argument('--batch_size', dest='batch_size', required= True, type=int, default=50, help='Batch size.')
+parser.add_argument('--z_dim', dest='z_dim', required= True, type=int, default=200, help='Latent space size.')
+parser.add_argument('--model', dest='model', type=str, default='PathologyGAN', help='Model name.')
 args = parser.parse_args()
 checkpoint = args.checkpoint
-num_samples = int(args.num_samples)
-batch_size = int(args.batch_size)
-
-model = 'StylePathologyGAN'
+num_samples = args.num_samples
+batch_size = args.batch_size
+z_dim = args.z_dim
+model = args.model
 
 main_path = os.path.dirname(os.path.realpath(__file__))
 dbs_path = os.path.dirname(os.path.realpath(__file__))
 
 # Dataset information.
-crimage_path = os.path.join(main_path, 'CRImage')
 data_out_path = os.path.join(main_path, 'data_model_output')
 data_out_path = os.path.join(data_out_path, model)
 image_width = 224
@@ -30,44 +31,25 @@ image_channels = 3
 dataset='vgh_nki'
 marker='he'
 offset = None
-if offset is not None:
-	name_run = 'h%s_w%s_n%s_%s' % (image_height, image_width, image_channels, offset)
-else:
-	name_run = 'h%s_w%s_n%s' % (image_height, image_width, image_channels)
+name_run = 'h%s_w%s_n%s' % (image_height, image_width, image_channels)
 data_out_path = '%s/%s' % (data_out_path, name_run)
 
 # Hyperparameters.
 learning_rate_g = 1e-4
 learning_rate_d = 1e-4
-learning_rate_e = 1e-4
 beta_1 = 0.5
 beta_2 = 0.9
 restore = False
 
 # Model
 layers = 5
-z_dim = 175
 alpha = 0.2
 n_critic = 5
 gp_coeff = .65
 use_bn = False
 loss_type = 'relativistic gradient penalty'
 
-
 data = Data(dataset=dataset, marker=marker, patch_h=image_height, patch_w=image_width, n_channels=image_channels, batch_size=batch_size, project_path=dbs_path)
 
 with tf.Graph().as_default():
-	# Instantiate StylePathologyGAN
     pathgan = PathologyGAN(data=data, z_dim=z_dim, layers=layers, use_bn=use_bn, alpha=alpha, beta_1=beta_1, learning_rate_g=learning_rate_g, learning_rate_d=learning_rate_d, beta_2=beta_2, n_critic=n_critic, gp_coeff=gp_coeff, loss_type=loss_type, model_name=model)
-
-    # Generate Fake samples from checkpoint.
-    gen_hdf5_path = generate_samples_from_checkpoint(model=pathgan, data=data, data_out_path=main_path, checkpoint=checkpoint, num_samples=num_samples, batches=batch_size)
-
-# Generate Inception features from fake images.
-with tf.Graph().as_default():
-    inception_tf_feature_activations(hdf5s=[gen_hdf5_path], input_shape=data.training.shape[1:], batch_size=50)
-
-# Generate CRImage features from fake images. 
-main_evaluation_path = gen_hdf5_path.split('hdf5_')[0]
-crimage_command = 'cd %s; Rscript --vanilla crimage_style.r %s' % (crimage_path, main_evaluation_path)
-os.system(crimage_command)
--- quantify_model_pipeline.py
+++ quantify_model_pipeline.py
@@ -4,28 +4,39 @@ import argparse
 from data_manipulation.data import Data
 from models.generative.gans.BigGAN import BigGAN
 from models.generative.gans.PathologyGAN import PathologyGAN
-from models.evaluation.features import *
 from models.score.score import Scores
 from models.score.crimage_score import CRImage_Scores
+from models.evaluation.features import *
 
 
 parser = argparse.ArgumentParser(description='StylePathologyGAN fake image generator and feature extraction.')
 parser.add_argument('--checkpoint', dest='checkpoint', required= True, help='Path to pre-trained weights (.ckt) of PathologyGAN.')
-parser.add_argument('--num_samples', dest='num_samples', required= True, type=int, help='Number of images to generate.')
-parser.add_argument('--batch_size', dest='batch_size', required= True, type=int, help='Batch size.')
-parser.add_argument('--z_dim', dest='z_dim', required= True, type=int, help='Latent space size.')
-# parser.add_argument('--create_samples', dest='create_samples', action='store_true', help='Boolean flag to control sample creation.')
+parser.add_argument('--num_samples', dest='num_samples', required= True, type=int, default=5000, help='Number of images to generate.')
+parser.add_argument('--batch_size', dest='batch_size', required= True, type=int, default=50, help='Batch size.')
+parser.add_argument('--z_dim', dest='z_dim', required= True, type=int, default=200, help='Latent space size.')
+parser.add_argument('--model', dest='model', type=str, default='PathologyGAN', help='Model name.')
 args = parser.parse_args()
 checkpoint = args.checkpoint
 num_samples = args.num_samples
 batch_size = args.batch_size
 z_dim = args.z_dim
-# create_samples = args.create_samples
+model = args.model
 
-model = 'StylePathologyGAN'
-
+# Paths for runs and datasets.
 main_path = os.path.dirname(os.path.realpath(__file__))
 dbs_path = os.path.dirname(os.path.realpath(__file__))
+
+# Real Image conv and CRImage features.
+nki_vgh_new_train = '%s/evaluation/real/nki_vgh/he/new/h224_w224_n3//hdf5_nki_vgh_he_features_train_real.h5' % main_path
+nki_vgh_new_test = '%s/evaluation/real/nki_vgh/he/new/h224_w224_n3/hdf5_nki_vgh_he_features_test_real.h5' % main_path
+nki_vgh_new_train_cr = '%s/evaluation/real/nki_vgh/he/new/h224_w224_n3//crimage_train.txt' % main_path
+nki_vgh_new_test_cr = '%s/evaluation/real/nki_vgh/he/new/h224_w224_n3/crimage_test.txt' % main_path
+
+print('Assuming the following files for the real images:')
+print('\t%s' % nki_vgh_new_train)
+print('\t%s' % nki_vgh_new_test)
+print('\t%s' % nki_vgh_new_train_cr)
+print('\t%s' % nki_vgh_new_test_cr)
 
 # Dataset information.
 crimage_path = os.path.join(main_path, 'CRImage')
@@ -36,39 +47,32 @@ image_height = 224
 image_channels = 3
 dataset='vgh_nki'
 marker='he'
-offset = None
-if offset is not None:
-	name_run = 'h%s_w%s_n%s_%s' % (image_height, image_width, image_channels, offset)
-else:
-	name_run = 'h%s_w%s_n%s' % (image_height, image_width, image_channels)
+name_run = 'h%s_w%s_n%s' % (image_height, image_width, image_channels)
 data_out_path = '%s/%s' % (data_out_path, name_run)
 
 # Hyperparameters.
 learning_rate_g = 1e-4
 learning_rate_d = 1e-4
-learning_rate_e = 1e-4
 beta_1 = 0.5
 beta_2 = 0.9
 restore = False
 
 # Model
 layers = 5
-# z_dim = 200
 alpha = 0.2
 n_critic = 5
 gp_coeff = .65
 use_bn = False
 loss_type = 'relativistic gradient penalty'
 
-
 data = Data(dataset=dataset, marker=marker, patch_h=image_height, patch_w=image_width, n_channels=image_channels, batch_size=batch_size, project_path=dbs_path)
 
 with tf.Graph().as_default():
-    if 'StylePathologyGAN' in checkpoint:
+    if 'PathologyGAN' in checkpoint:
     	# Instantiate StylePathologyGAN
         pathgan = PathologyGAN(data=data, z_dim=z_dim, layers=layers, use_bn=use_bn, alpha=alpha, beta_1=beta_1, learning_rate_g=learning_rate_g, learning_rate_d=learning_rate_d, beta_2=beta_2, n_critic=n_critic, gp_coeff=gp_coeff, loss_type=loss_type, model_name=model)
 
-    elif 'PathologyGAN' in checkpoint:
+    elif 'BigGAN' in checkpoint:
         # Instantiate PathologyGAN
         pathgan = BigGAN(data=data, z_dim=z_dim, layers=layers, use_bn=use_bn, alpha=alpha, beta_1=beta_1, learning_rate_g=learning_rate_g, learning_rate_d=learning_rate_d, beta_2=beta_2, n_critic=n_critic, gp_coeff=gp_coeff, conditional=False, label_dim=None, label_t=None, loss_type=loss_type, model_name='PathologyGAN')
 
@@ -79,19 +83,12 @@ with tf.Graph().as_default():
 with tf.Graph().as_default():
     hdf5s_features = inception_tf_feature_activations(hdf5s=[gen_hdf5_path], input_shape=data.training.shape[1:], batch_size=50)
 
-
 # Generate CRImage features from fake images. 
 main_evaluation_path = gen_hdf5_path.split('hdf5_')[0]
 crimage_file = os.path.join(main_evaluation_path, 'crimage_train.txt')
 if not os.path.isfile(crimage_file):
 	crimage_command = 'cd %s; Rscript --vanilla crimage_style.r %s %s' % (crimage_path, main_evaluation_path, num_samples)
 	os.system(crimage_command)
-
-nki_vgh_new_train = '/media/adalberto/Disk2/Cancer_TMA_Generative/evaluation/real/nki_vgh/he/new/h224_w224_n3//hdf5_nki_vgh_he_features_train_real.h5'
-nki_vgh_new_test = '/media/adalberto/Disk2/Cancer_TMA_Generative/evaluation/real/nki_vgh/he/new/h224_w224_n3/hdf5_nki_vgh_he_features_test_real.h5'
-
-nki_vgh_new_train_cr = '/media/adalberto/Disk2/Cancer_TMA_Generative/evaluation/real/nki_vgh/he/new/h224_w224_n3//crimage_train.txt'
-nki_vgh_new_test_cr = '/media/adalberto/Disk2/Cancer_TMA_Generative/evaluation/real/nki_vgh/he/new/h224_w224_n3/crimage_test.txt'
 
 print('----- INCEPTION FEATURES -----')
 print('Study on VGH_NKI')
